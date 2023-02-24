49f4fd5203278e0741c73806195dbbacc00fd597
Agustin Moran <agustin.moranr@gmail.com>
2023-02-24 17:49:45 -0600

    bcakend: modifica implementación para utilizar libreria serverless-express y poder desplegar backend en aws

--- backend/src/app.js
+++ backend/src/app.js
@@ -119,6 +119,11 @@ app.use(
 	}),
 );
 
-app.listen(PORT, () =>
-	console.log(`Server running successfully in port:${PORT}`),
-);
+// The serverless-express library creates a server and listens on a Unix
+// Domain Socket for you, so you can remove the usual call to app.listen.
+// app.listen(PORT, () =>
+// 	console.log(`Server running successfully in port:${PORT}`),
+// );
+
+// Export your express server so you can import it in the lambda function.
+module.exports = app;
